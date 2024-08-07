1d139527ba35ddcb3b0dc108b348cee089ff06dd
minseokkim <minseok5027@cau.ac.kr>
2024-08-08 00:52:22 +0900

    엔티티 설계

---
+++ msblog/src/main/java/com/test/msblog/entity/User.java
@@ -0,0 +1,38 @@
+package com.test.msblog.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 4, max = 15, message = "작성자명은 4자 이상 15자 이하만 가능합니다.")
+    @Pattern(regexp = "^[a-z0-9]*$", message = "작성자명은 알파벳 소문자와 숫자로만 구성되어야 합니다.")
+    private String username;
+
+    @Column(nullable = false)
+    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하만 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "비밀번호는 알파벳 대소문자와 숫자로만 구성되어야 합니다.")
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+}
