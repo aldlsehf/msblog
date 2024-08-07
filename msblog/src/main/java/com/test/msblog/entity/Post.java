57b59322c09b76aa53835a00625d76f3593d2b5e
minseokkim <minseok5027@cau.ac.kr>
2024-08-08 01:19:07 +0900

    Post에 생성자랑 Date변수 추가

--- msblog/src/main/java/com/test/msblog/entity/Post.java
+++ msblog/src/main/java/com/test/msblog/entity/Post.java
@@ -4,10 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @NoArgsConstructor
 public class Post {
+
+    public Post(String title, String username, String contents, User user) {
+        this.title = title;
+        this.username = username;
+        this.contents = contents;
+        this.user = user;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +31,24 @@ public class Post {
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
+    @Column(name = "createDate", nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @Column(name = "modifiedDate", nullable = false)
+    private LocalDateTime modifiedDate;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        createDate = LocalDateTime.now();
+        modifiedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        modifiedDate = LocalDateTime.now();
+    }
 }
